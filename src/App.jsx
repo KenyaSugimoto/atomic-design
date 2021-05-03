177609a15ddbd71808673bf65062212e218337ce
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-05-03 15:37:59 +0900

    テンプレート

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { BrowserRouter } from "react-router-dom";
 import { SearchInput } from "../components/molecules/SearchInput";
 import { UserCard } from "../components/organisms/user/UserCard";
+import { DefaultLayout } from "../components/templates/DefaultLayout";
+import { HeaderOnly } from "../components/templates/HeaderOnly";
 import "./styles.css";
 
 export default function App() {
@@ -11,9 +14,11 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }

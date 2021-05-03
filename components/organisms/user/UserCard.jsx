bfe4945c45021a0f0962dbb4f39b06dc6c3523a1
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-05-03 15:13:16 +0900

    カードの作成

---
+++ components/organisms/user/UserCard.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Card } from "../../atoms/card/Card";
+import { UserIconWithName } from "../../molecules/user/UserIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.mail}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { SearchInput } from "../components/molecules/SearchInput";
+import { UserCard } from "../components/organisms/user/UserCard";
+import "./styles.css";
 
 export default function App() {
+  const user = {
+    image: "https://source.unsplash.com/xgTMSz6kegE",
+    name: "けんや",
+    mail: "test@ritsumei.ac.jp",
+    phone: "0977-23-1234"
+  };
+
   return (
     <div className="App">
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }

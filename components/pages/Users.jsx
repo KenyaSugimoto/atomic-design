2631f9d3878abf84613db3e44dec57d2e3b7ed94
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-05-03 16:40:44 +0900

    atomic design

---
+++ components/pages/Users.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `Kenya${val}`,
+    image: "https://source.unsplash.com/gKXKBY-C-Dk",
+    mail: "test@yahoo.co.jp",
+    phone: "0977-23-2345"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザ一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { SearchInput } from "../components/molecules/SearchInput";
-import { UserCard } from "../components/organisms/user/UserCard";
-import { DefaultLayout } from "../components/templates/DefaultLayout";
-import { HeaderOnly } from "../components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  const user = {
-    image: "https://source.unsplash.com/xgTMSz6kegE",
-    name: "けんや",
-    mail: "test@ritsumei.ac.jp",
-    phone: "0977-23-1234"
-  };
-
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
